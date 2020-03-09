d4199a47260b2c20092479394622a600f3d96258
nn-nito <retou.1007@gmail.com>
2020-03-09 15:38:04 +0000

    タグ：デザイン修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,23 +103,23 @@
             <div class="main-title">
                 <h3><span>人気のタグ</span></h3>
             </div>
-            <div class="tag">
-                <a href="#" class="badge badge-primary"><h5>風景</h5></a>
-                <a href="#" class="badge badge-secondary"><h5>自然</h5></a>
-                <a href="#" class="badge badge-success"><h5>Success</h5></a>
-                <a href="#" class="badge badge-danger"><h5>Success</h5></a>
-                <a href="#" class="badge badge-warning"><h5>Success</h5></a>
-                <a href="#" class="badge badge-info"><h5>Success</h5></a>
-                <a href="#" class="badge badge-light"><h5>Success</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
-                <a href="#" class="badge badge-dark"><h5>Dark</h5></a>
+            <div class="tagtag">
+                <a href="#">風景</a>
+                <a href="#">自然</a>
+                <a href="#">Success</a>
+                <a href="#">Success</a>
+                <a href="#">Success</a>
+                <a href="#">Success</a>
+                <a href="#">Success</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
+                <a href="#">Dark</a>
             </div>
         </div>
     </div>
