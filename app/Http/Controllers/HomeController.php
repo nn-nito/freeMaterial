39ac461444fbffbeba15e05f47c3dada4e89549d
nn-nito <retou.1007@gmail.com>
2020-03-15 12:25:28 +0000

    リファクタリング：ページネーションの数定数化

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use App\Http\Services\TagService;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use phpDocumentor\Reflection\Types\Self_;
 
 /**
  * Class HomeController
@@ -17,24 +18,36 @@ use Illuminate\View\View;
 class HomeController extends Controller
 {
 	/**
+	 * 最新の写真のページネーション数
+	 *
+	 * @var int
+	 */
+	private const LATEST_PHOTO_PAGINATION_COUNT = 30;
+	private const INFO_PAGINATION_COUNT = 5;
+	private const POPULAR_PHOTO_PAGINATION_COUNT = 5;
+	private const TAG_PAGINATION_COUNT = 30;
+
+
+
+	/**
 	 * @return Factory|View
 	 */
 	public function index()
 	{
 		// お知らせ取得
 		$info_service = InfoService::create();
-		$infos = $info_service->getCollectionByLatest(5);
+		$infos = $info_service->getCollectionByLatest(self::INFO_PAGINATION_COUNT);
 
 		// 最新の写真を取得
 		$photo_service = PhotoService::create();
-		$photos = $photo_service->getPaginationByLatestMonth(6);
+		$photos = $photo_service->getPaginationByLatestMonth(self::LATEST_PHOTO_PAGINATION_COUNT);
 
 		// 人気の写真を取得
-		$popular_photos = $photo_service->getCollectionByPeriod('', 5);
+		$popular_photos = $photo_service->getCollectionByPeriod('', self::POPULAR_PHOTO_PAGINATION_COUNT);
 
 		// タグを取得
 		$tag_service = TagService::create();
-		$tags = $tag_service->getAllCollectionBySortPopular(30);
+		$tags = $tag_service->getAllCollectionBySortPopular(self::TAG_PAGINATION_COUNT);
 
 		return view('home', compact('infos', 'photos', 'popular_photos', 'tags'));
 	}
